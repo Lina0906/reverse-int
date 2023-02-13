1c2a93479f5393f77ff82f50b5c8c16f5f55c822
Lina Churbanova <lina.moiseeva@gmail.com>
2023-02-13 22:40:32 +0400

    fix: fix reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    
+    if (n < 0) {n = n*-1};
+    {n};
+
+    const stringNumber = n.toString();
+    let reversedStringArr = [];
+
+    for (let i=0; i<=stringNumber.length-1; i++) {
+        reversedStringArr.push(stringNumber[stringNumber.length-1-i]);
+    }
+
+    return reversedStringArr.join('') * 1;
 }
+
+
